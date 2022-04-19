a890258aff0e0a593d5abefd9d7c035d5c40537e
Mathews <o.maatth@gmail.com>
2022-04-19 18:46:50 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2 #Criando atributo default fora do __init__, para atributos q não mudam muito
+
     def __init__(self, *filhos, nome = None, idade = 26):
         self.idade = idade
         self.nome = nome
@@ -19,5 +21,12 @@ if __name__ == '__main__':
         print(filho.nome)
     luciano.sobrenome = 'Ramalho' #adicionando um atributo especifico para a def criada, serve apenas para este nome.
     del luciano.filhos
+    luciano.olhos = 1
+    del luciano.olhos #apagando apenas o atributo que foi dado acima
     print(luciano.__dict__)
-    print(mathews.__dict__)
+    print(mathews.__dict__) #atributo de instancia, predefinidos dentro do __init__ ou criadas dentro do processo
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(mathews.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(mathews.olhos))
